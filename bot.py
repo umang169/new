5ac163306739068b0875c37ee2d486f150327147
umang169 <90959310+umang169@users.noreply.github.com>
2021-10-01 18:20:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -144,7 +144,7 @@ async def remove_username(new_List):
 async def addFooter(str):
     footer = """
 ━━━━━━━━━━━━━━━
-⭐️JOIN CHANNEL ➡️ t.me/""" + CHANNEL
+⭐️JOIN CHANNEL ➡️ /""" + CHANNEL
     return str + footer
 
 bot.run()
